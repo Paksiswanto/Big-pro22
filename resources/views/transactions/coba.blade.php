43791d0d2ddcf3d94a689be663415ec907ccea97
TrisqiTegarValenzi <trisqitegarv@gmail.com>
2023-06-15 08:13:43 +0700

    perbaikan hapus akun

--- resources/views/transactions/coba.blade.php
+++ resources/views/transactions/coba.blade.php
@@ -1,23 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Convert Currency</title>
+    <title>Currency Conversion</title>
 </head>
 <body>
-    <h1>Convert Currency</h1>
-    
-    <form action="/convertCurrency" method="POST">
-        @csrf
-        <label for="amount">Amount (IDR):</label>
-        <input type="number" name="amount" id="amount" required>
-        <button type="submit">Convert</button>
-    </form>
-    
-    <h2>Conversion Result</h2>
-    @foreach($result as $row)
-        <p>Original Amount: {{ '$row->original_amount' }} IDR</p>
-        <p>Converted Amount: {{ '$row->converted_amount' }} USD</p>
-        <p>Exchange Rate: 1 USD = {{ '$row->exchange_rate' }} IDR</p>
-    @endforeach
+    <h1>Currency Conversion Result</h1>
+
+    <p>{{ $amount }} {{ $baseCurrency }} = {{ $result }} {{ $targetCurrency }}</p>
 </body>
 </html>
